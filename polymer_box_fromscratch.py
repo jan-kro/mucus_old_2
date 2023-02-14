dbbeaa2483d637abfd1da5ecab6c9e29f7264822
jan-kro <jan.kroissenbrunner@gmail.com>
2023-02-14 19:34:49 +0100

    implemented simulation through config file

--- polymer_box_fromscratch.py
+++ polymer_box_fromscratch.py
@@ -1,8 +1,7 @@
 from polymer_chain import Polymer
-from time import time
+import time
+import datetime
 import os
-
-
 
 
 n_beeds = 10
@@ -11,10 +10,14 @@ mobility = 0.0005
 r_0 = 2
 l_box = n_beeds*r_0
 
-steps = 500000
+steps = 5000
 stride = 50
 
 fname = f"/storage/janmak98/masterthesis/trajectories/box/Traj_{n_chains:d}Chains_{n_beeds:d}BeadsPerChain_{steps:d}Steps_{mobility:.6f}mu_{stride:d}Stride.gro"
+
+now = time.localtime()
+now_str = f"{now.tm_mon:d}.{now.tm_mday}.{now.tm_year}  {now.tm_hour}:{now.tm_min}:{now.tm_sec}"
+print("simulation started ", now_str)
 
 p = Polymer(mobility=mobility,
             cwd=os.getcwd())
@@ -22,15 +25,21 @@ p.create_box(N=n_beeds,
              M=n_chains,
              L=l_box)
 
-t1 = time()
+t1 = time.time()
 p.simulate(steps=steps, 
            stride=stride)
-t2 = time()
+t2 = time.time()
 
-print("simulation time")
-print(f"{(t2-t1)//60:.0f}:{(t2-t1)%60:2.0f} min")
+now = time.localtime()
+now_str = f"{now.tm_mon:d}.{now.tm_mday}.{now.tm_year}  {now.tm_hour}:{now.tm_min}:{now.tm_sec}"
 
+print("simulation finished ", now_str)
+
+print("simulation time: ", datetime.timedelta(seconds=round(t2-t1)))
+#print(f"{(t2-t1)//60:.0f}:{(t2-t1)%60:2.0f} min")
 #p.plot_distance_distribution()
 #p.save_traj_gro(fname_traj=fname)
+print("save trajectroy ...")
+p.save_traj_gro()
 print("Done")
 
