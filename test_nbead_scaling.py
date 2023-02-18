fdd5643025b62bd3a53c2577b46a142662b5f867
jan-kro <jan.kroissenbrunner@gmail.com>
2023-02-18 14:53:56 +0100

    testing again

--- test_nbead_scaling.py
+++ test_nbead_scaling.py
@@ -3,9 +3,9 @@ import os
 
 cwd = os.getcwd()
 cfg_path = cwd + "/configs/tests"
-n = 6
+n = 5
 for i in range(n):
-    fin = cfg_path + f"/cfg_test_time_nbeads_{i:d}.toml"
+    fin = cfg_path + f"/cfg_test_time_nbeads_2_{i:d}.toml"
     
     print(f"\n\nRunning simulation {i+1:d}/{n:d} ...\n")
     run_sim(fname_config=fin)
