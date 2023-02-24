637e69ab715046f482171cf7d3c5addb62720673
jan-kro <jan.kroissenbrunner@gmail.com>
2023-02-24 01:58:44 +0100

    fixed fname

--- polymer_box_testCharges.py
+++ polymer_box_testCharges.py
@@ -3,5 +3,5 @@ import os
 
 n = 6
 for k in range(n):
-    fname = os.getcwd() + f"configs/tests/box_testCharges_100beads_1Msteps_{k:d}.toml"
+    fname = os.getcwd() + f"/configs/tests/box_testCharges_100beads_1Msteps_{k:d}.toml"
     run_sim(type="box", fname_config=fname)
--- polymer_chain_testForces.py
+++ polymer_chain_testForces.py
@@ -3,5 +3,5 @@ import os
 
 n = 4
 for k in range(n):
-    fname = os.getcwd() + f"configs/tests/chain_testForces_50beads_1Msteps_{k:d}.toml"
+    fname = os.getcwd() + f"/configs/tests/chain_testForces_50beads_1Msteps_{k:d}.toml"
     run_sim(type="box", fname_config=fname)
